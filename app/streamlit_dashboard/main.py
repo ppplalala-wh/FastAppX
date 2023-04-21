3b063a78982dcef9cd44ecff426a2c6ba3dd1376
wenhao.liao <wenhao.liao@siemens.com>
2023-04-21 13:09:23 +0800

    WIP

--- app/streamlit_dashboard/main.py
+++ app/streamlit_dashboard/main.py
@@ -8,3 +8,16 @@ import numpy as np
 import plotly.figure_factory as ff
 import plotly.express as px
 import plotly.graph_objects as go
+
+reliability_url = 'http://localhost:8080/'
+
+st.title('Long Term Reliability Model')
+st.text('In REAMS, long term reliability for train system is measured by failure per \nkilometers /'
+        '(FPMK). Simulate and forecast long term reliability is of \ngreat importance in renewal /'
+        'planning')
+
+uploaded_file = st.file_uploader('upload sample FPMK history')
+
+fpmk_hist = pd.read_csv(uploaded_file)
+st.dataframe(fpmk_hist.head())
+
